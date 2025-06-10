0ba9ed2157b74a5077f98b8b7c0f5499790525a5
Markus Ecker <markus.ecker@gmail.com>
2025-06-10 15:50:09 +0200

    update CLI name

--- typescript-sdk/packages/cli/src/index.ts
+++ typescript-sdk/packages/cli/src/index.ts
@@ -55,7 +55,7 @@ async function createProject() {
   });
 }
 
-program.name("ag-ui-create").description("AG-UI CLI").version("0.0.1-alpha.0");
+program.name("create-ag-ui-app").description("AG-UI CLI").version("0.0.1-alpha.0");
 
 program.action(async () => {
   await createProject();
